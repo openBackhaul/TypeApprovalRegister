ad9d2aee538be7beac7166c3f5bb851f61df379e
Prathiba <prathiba.jeevan.external@telefonica.com>
2022-04-19 00:30:06 +0200

    Add OAM services implementation Fixes #12

--- server/controllers/ApplicationProfile.js
+++ server/controllers/ApplicationProfile.js
@@ -1,44 +1,79 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
 var ApplicationProfile = require('../service/ApplicationProfileService');
+var authorizingService = require('onf-core-model-ap-bs/basicServices/AuthorizingService');
+var responseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/ResponseBuilder');
+var responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server/ResponseCode');
+var oamLogService = require('onf-core-model-ap-bs/basicServices/OamLogService');
 
-module.exports.getApplicationProfileApplicationName = function getApplicationProfileApplicationName (req, res, next, uuid) {
-  ApplicationProfile.getApplicationProfileApplicationName(uuid)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.getApplicationProfileApplicationName = async function getApplicationProfileApplicationName(req, res, next, uuid) {
+  let responseCode = responseCodeEnum.code.OK;
+  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
+    await ApplicationProfile.getApplicationProfileApplicationName(req.url)
+      .then(function (response) {
+        responseBuilder.buildResponse(res, responseCode, response);
+      })
+      .catch(function (response) {
+        responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+        responseBuilder.buildResponse(res, responseCode, response);
+      });
+  } else {
+    responseCode = responseCodeEnum.code.UNAUTHORIZED;
+    responseBuilder.buildResponse(res, responseCode);
+  }
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.getApplicationProfileApprovalStatus = function getApplicationProfileApprovalStatus (req, res, next, uuid) {
-  ApplicationProfile.getApplicationProfileApprovalStatus(uuid)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.getApplicationProfileApprovalStatus = async function getApplicationProfileApprovalStatus(req, res, next, uuid) {
+  let responseCode = responseCodeEnum.code.OK;
+  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
+    await ApplicationProfile.getApplicationProfileApprovalStatus(req.url)
+      .then(function (response) {
+        responseBuilder.buildResponse(res, responseCode, response);
+      })
+      .catch(function (response) {
+        responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+        responseBuilder.buildResponse(res, responseCode, response);
+      });
+  } else {
+    responseCode = responseCodeEnum.code.UNAUTHORIZED;
+    responseBuilder.buildResponse(res, responseCode);
+  }
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.getApplicationProfileReleaseNumber = function getApplicationProfileReleaseNumber (req, res, next, uuid) {
-  ApplicationProfile.getApplicationProfileReleaseNumber(uuid)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.getApplicationProfileReleaseNumber = async function getApplicationProfileReleaseNumber(req, res, next, uuid) {
+  let responseCode = responseCodeEnum.code.OK;
+  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
+    await ApplicationProfile.getApplicationProfileReleaseNumber(req.url)
+      .then(function (response) {
+        responseBuilder.buildResponse(res, responseCode, response);
+      })
+      .catch(function (response) {
+        responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+        responseBuilder.buildResponse(res, responseCode, response);
+      });
+  } else {
+    responseCode = responseCodeEnum.code.UNAUTHORIZED;
+    responseBuilder.buildResponse(res, responseCode);
+  }
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
-module.exports.putApplicationProfileApprovalStatus = function putApplicationProfileApprovalStatus (req, res, next, body, uuid) {
-  ApplicationProfile.putApplicationProfileApprovalStatus(body, uuid)
-    .then(function (response) {
-      utils.writeJson(res, response);
-    })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-    });
+module.exports.putApplicationProfileApprovalStatus = async function putApplicationProfileApprovalStatus(req, res, next, body, uuid) {
+  let responseCode = responseCodeEnum.code.NO_CONTENT;
+  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
+    await ApplicationProfile.putApplicationProfileApprovalStatus(body, req.url)
+      .then(function (response) {
+        responseBuilder.buildResponse(res, responseCode, response);
+      })
+      .catch(function (response) {
+        responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+        responseBuilder.buildResponse(res, responseCode, response);
+      });
+  } else {
+    responseCode = responseCodeEnum.code.UNAUTHORIZED;
+    responseBuilder.buildResponse(res, responseCode);
+  }
+  oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
