64abcfb8d5687fe04749c9fc2a85075380f1eebe
Prathiba <prathiba.jeevan.external@telefonica.com>
2022-05-12 07:44:29 +0200

    Refactor : the security validation machanism for apiKeyAuth and basicAuth validations Fixes #29

--- server/controllers/ApplicationProfile.js
+++ server/controllers/ApplicationProfile.js
@@ -8,8 +8,7 @@ var oamLogService = require('onf-core-model-ap-bs/basicServices/OamLogService');
 
 module.exports.getApplicationProfileApplicationName = async function getApplicationProfileApplicationName(req, res, next, uuid) {
   let responseCode = responseCodeEnum.code.OK;
-  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
-    await ApplicationProfile.getApplicationProfileApplicationName(req.url)
+  await ApplicationProfile.getApplicationProfileApplicationName(req.url)
       .then(function (response) {
         responseBuilder.buildResponse(res, responseCode, response);
       })
@@ -17,17 +16,12 @@ module.exports.getApplicationProfileApplicationName = async function getApplicat
         responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
         responseBuilder.buildResponse(res, responseCode, response);
       });
-  } else {
-    responseCode = responseCodeEnum.code.UNAUTHORIZED;
-    responseBuilder.buildResponse(res, responseCode);
-  }
   oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
 module.exports.getApplicationProfileApprovalStatus = async function getApplicationProfileApprovalStatus(req, res, next, uuid) {
   let responseCode = responseCodeEnum.code.OK;
-  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
-    await ApplicationProfile.getApplicationProfileApprovalStatus(req.url)
+  await ApplicationProfile.getApplicationProfileApprovalStatus(req.url)
       .then(function (response) {
         responseBuilder.buildResponse(res, responseCode, response);
       })
@@ -35,17 +29,12 @@ module.exports.getApplicationProfileApprovalStatus = async function getApplicati
         responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
         responseBuilder.buildResponse(res, responseCode, response);
       });
-  } else {
-    responseCode = responseCodeEnum.code.UNAUTHORIZED;
-    responseBuilder.buildResponse(res, responseCode);
-  }
   oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
 module.exports.getApplicationProfileReleaseNumber = async function getApplicationProfileReleaseNumber(req, res, next, uuid) {
   let responseCode = responseCodeEnum.code.OK;
-  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
-    await ApplicationProfile.getApplicationProfileReleaseNumber(req.url)
+  await ApplicationProfile.getApplicationProfileReleaseNumber(req.url)
       .then(function (response) {
         responseBuilder.buildResponse(res, responseCode, response);
       })
@@ -53,17 +42,12 @@ module.exports.getApplicationProfileReleaseNumber = async function getApplicatio
         responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
         responseBuilder.buildResponse(res, responseCode, response);
       });
-  } else {
-    responseCode = responseCodeEnum.code.UNAUTHORIZED;
-    responseBuilder.buildResponse(res, responseCode);
-  }
   oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
 
 module.exports.putApplicationProfileApprovalStatus = async function putApplicationProfileApprovalStatus(req, res, next, body, uuid) {
   let responseCode = responseCodeEnum.code.NO_CONTENT;
-  if (await authorizingService.isAuthorized(req.headers.authorization, req.method)) {
-    await ApplicationProfile.putApplicationProfileApprovalStatus(body, req.url)
+  await ApplicationProfile.putApplicationProfileApprovalStatus(body, req.url)
       .then(function (response) {
         responseBuilder.buildResponse(res, responseCode, response);
       })
@@ -71,9 +55,5 @@ module.exports.putApplicationProfileApprovalStatus = async function putApplicati
         responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
         responseBuilder.buildResponse(res, responseCode, response);
       });
-  } else {
-    responseCode = responseCodeEnum.code.UNAUTHORIZED;
-    responseBuilder.buildResponse(res, responseCode);
-  }
   oamLogService.recordOamRequest(req.url, req.body, responseCode, req.headers.authorization, req.method);
 };
